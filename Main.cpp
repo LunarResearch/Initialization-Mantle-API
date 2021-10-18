3bddc50dc00b58b3720e95237db14883d41365cb
LunarResearch <tesla707@mail.ru>
2021-10-18 21:46:13 +0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,29 @@
+#include "grApi.h"
+
+
+#ifdef _UNICODE
+#define tWinMain wWinMain
+#else
+#define tWinMain WinMain
+#endif // _UNICODE
+
+
+int APIENTRY tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PTCHAR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Разместите код здесь.
+	grApi gr;
+
+	UpdateWindow(gr.hWnd);
+
+	MSG msg{};
+	while (msg.message != WM_QUIT)
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+	return static_cast<GR_INT>(msg.wParam);
+}
